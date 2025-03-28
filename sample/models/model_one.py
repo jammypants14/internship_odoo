62219e72225744372af30d248c44967559bf2cd3
jishnu <jishnuajayan.m14@gmail.com>
2025-03-28 18:27:02 +0530

    new views and menus

--- sample/models/model_one.py
+++ sample/models/model_one.py
@@ -5,9 +5,9 @@ class ModelOne(models.Model):
 	_name = "model.one"
 	_description = "Model One"
 	
-	name = fields.Char(string="Name", help='A normal name field')
-	age = fields.Integer(string="Age")
-	gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('other', 'Other')], string="Gender")
+	name = fields.Char(string="Name", help='You can add your name here', copy=False)
+	age = fields.Integer(string="Age", default=18)
+	gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('other', 'Other')], string="Gender", required=True, copy=False)
 	active = fields.Boolean('Active')
-	description = fields.Text("Description")
+	description = fields.Text("Description", default="Test Description")
 	date = fields.Date("Date")
